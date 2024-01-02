7e48efabafd818dca4a39b07431ae02c075628c3
Ivan Vukusic <jaaajaa102@gmail.com>
2024-01-02 14:28:04 -0500

    Clean up

--- create_anki.py
+++ create_anki.py
@@ -1,14 +1,19 @@
 import genanki
-from random import seed
-from random import randint
-import uuid
 import os
 from utils.speech import synthesize_speech, synthesize_template_speech
 from utils.constants import kofi_css, templates
 from utils.helpers import parse_verb_file, get_tags, make_card
 
+
 def get_sentence_template(tense, form, infinitive):
-    return templates.get(tense, "").format(form, infinitive)
+    sentence_template = templates.get(tense, "").format(form, infinitive)
+    if tense == "Infinitive":
+        sentence = sentence_template.replace(
+                '[...]', f'{{c1::{tense}}}')
+    else:
+        sentence = sentence_template.replace(
+                '[...]', f'{{c1::{form}}}')
+    return sentence
 
 
 def create_anki_deck(verb_data):
@@ -22,7 +27,8 @@ def create_anki_deck(verb_data):
         templates=[{
             "name": "Cloze",
             "qfmt": "{{cloze:Prompt}}<br>{{Sound}}",
-            "afmt": "{{cloze:Prompt}}<br>Notes:<br>{{Notes}}<br>{{Sound}}<br>Template Audio:{{TemplateAudio}}"
+            "afmt": """{{cloze:Prompt}}<br>Notes:<br>{{Notes}}
+            <br>{{Sound}}<br>Template Audio:{{TemplateAudio}}"""
         }],
         css=kofi_css,
         model_type=genanki.Model.CLOZE
@@ -36,61 +42,52 @@ def create_anki_deck(verb_data):
             word_folder = os.path.join("audio_files", verb_name)
             os.makedirs(word_folder, exist_ok=True)
 
-            # file.write(f"{verb_name}\n{note}\n{conjugations}")
             file.write(f"VERB: {verb_name}\n")
             file.write(f"NOTE: {note}\n")
-            print(conjugations.items())
             for tense, conjs in conjugations.items():
 
+                # PARTIZIP I and PARTIZIP II
                 if tense in ["Partizip I", "Partizip II"]:
                     # Split the conjugations into individual forms
                     forms = conjs.split()
 
                     for form in forms:
                         form = form.translate({ord(i): None for i in '!,'})
-                        sentence_template = get_sentence_template(tense,form,verb_name)
-                        sentence = sentence_template.replace(
-                            '[...]', f'{{c1::{form}}}')
-
+                        sentence = get_sentence_template(tense,
+                                                         form, verb_name)
                         file.write(f"{sentence}\n")
                         tags = get_tags(tense, verb_name)
                         make_card(my_deck, kofi_model, sentence, note, tags)
                 else:
-                    forms = conjs.split()
-                    print(f"forms other: {forms}")
 
+                    forms = conjs.split()
                     for i in range(0, len(forms), 2):
                         person = forms[i].translate({ord(i): None for i in '!,'})
                         form = forms[i+1].translate({ord(i): None for i in '!,'})
+                        # IMPERATIVS
                         if tense == "Imperativs":
-                            singular = forms[i].translate({ord(i): None for i in '!,'})
-                            plural = forms[i+1].translate({ord(i): None for i in '!,'})
+                            singular = person
+                            plural = form
                             for i, imperativ in enumerate([singular, plural]):
-                                sentence_template = get_sentence_template(tense, imperativ, verb_name)
-                                sentence = sentence_template.replace(
-                                    '[...]', f'{{c1::{imperativ}}}')
-
+                                sentence = get_sentence_template(tense,imperativ, verb_name)
                                 subject = "du" if i == 0 else "ihr/Sie"
                                 tags = get_tags(tense, verb_name, subject)
                                 make_card(my_deck, kofi_model, sentence, note, tags)
                                 file.write(f"{sentence}\n")
                             continue
 
+                        # PRASENS, Konjunktiv II: Präteritum,
+                        # Indikativ: Präteritum
                         word = person + " " + form
-                        sentence_template = get_sentence_template(tense, word, verb_name)
-                        sentence = sentence_template.replace(
-                            '[...]', f'{{c1::{word}}}')
-
+                        sentence = get_sentence_template(tense, word, verb_name)
                         file.write(f"{sentence}\n")
                         tags = get_tags(tense, verb_name, person)
-                        print(tags, "PRASENS")
                         make_card(my_deck, kofi_model, sentence, note, tags)
-
-            # INFINITIVE
-            third_person_present = conjugations['Präsens'].split()[5]  # Get the third word which is the 3rd person form
-            sentence_template = get_sentence_template("Infinitiv", verb_name, third_person_present)
-            sentence = sentence_template.replace(
-                '[...]', f'{{c1::{verb_name}}}')
+            # INFINITIVS
+            # Get the third word which is the 3rd person form
+            third_person_present = conjugations['Präsens'].split()[5]
+            sentence = get_sentence_template("Infinitiv",
+                                                      verb_name, third_person_present)
             file.write(f"{sentence}\n")
             tags = get_tags("Infinitiv", verb_name)
             make_card(my_deck, kofi_model, sentence, note, tags)
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,7 @@
 import uuid
 import genanki
 
+
 def parse_verb_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
@@ -12,7 +13,8 @@ def parse_verb_file(filename):
     for verb in verbs:
         lines = verb.strip().split("\n")
         if lines:
-            # Extract verb name from the first line, assuming it's formatted as 'zu [verb]'
+            # Extract verb name from the first line,
+            # assuming it's formatted as 'zu [verb]'
             verb_name = lines[0].strip().split('zu ')[-1].strip()
 
             note = ""
@@ -26,25 +28,38 @@ def parse_verb_file(filename):
                     conjugations[tense.strip()] = conj.strip()
 
             verb_data.append((verb_name, note, conjugations))
-
     return verb_data
 
+
 def get_tags(tense, verb, subject=None):
+
+    if tense == "Ind. Präteritum":
+        tense = "Indikativ Präteritum"
+    elif tense == "Sub. Präteritum":
+        tense = "Konjunktiv II Präteritum"
     tense = tense.replace(" ", "_")
     tags = [f"tag:{tense}", f"tag:{verb}"]
+
     if verb.endswith("en"):
         tags.append("tag:ends_in_en")
     elif verb.endswith("ern"):
         tags.append("tag:ends_in_ern")
     elif verb.endswith("eln"):
         tags.append("tag:ends_in_eln")
+
     if subject is not None:
         tags.append(f"tag:{subject}")
+
+    modal_verbs = ["können", "müssen", "wollen", "sollen", "dürfen", "mögen"]
+
+    if verb in modal_verbs:
+        tags.append("tag:modal_verb")
     return tags
 
 
-def make_card(deck, model, sentence, note, tags, sound=None, template_audio=None):
+def make_card(deck, model, sentence, note, tags,
+              sound=None, template_audio=None):
     my_note = genanki.Note(model=model, fields=[
         sentence, str(uuid.uuid4()), note, "sound", "template_audio"],
-        tags=tags)
+                           tags=tags)
     deck.add_note(my_note)
